a8d72c4ca6c40959e9bff1e9dd8fffe6dac73f12
Ahmad Arif <ahmad.arif019@gmail.com>
2017-03-02 15:24:22 +0700

    tambah opsi lain untuk logout handler

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by ARIF on 28-Feb-17.
@@ -17,21 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/login", "/logout", "/tes").permitAll()
+                .antMatchers("/", "/tes", "/login").permitAll()
                 .antMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
 
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessHandler(new LogoutSuccess())
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .permitAll()
+//                .and().logout().logoutSuccessHandler(new LogoutSuccess()).permitAll() // logout ok, gak bisa langsung login, kecuali hit ke yang butuh role terlebih dahulu
+                .and().logout().permitAll() // logout error 403, tapi auth aplikasi berjalan normal
 
-                .and()
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
 }
